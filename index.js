c1ef1caa94d209051f9c60b98d9e9d3f20e9024c
Mohamed <mohasalanka2020@gmail.com>
2023-03-13 17:33:50 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser='Moha Salad';
+
+const welcomeMessage=`Welcome to Flatbook, + ${currentUser}!`;
+
+const excitedWelcomeMessage=welcomeMessage.toUpperCase(`Welcome to Flatbook, + ${currentUser}!`);
+
+const shortGreeting=`Welcome, + ${currentUser.slice(0,1)}!`;
